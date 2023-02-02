0b067a134404dcc13839a6c6a33bf21e76b1ea63
MertG <mertdeneme10@outlook.com>
2023-02-02 19:56:55 +0300

    second draft

--- javascript.js
+++ javascript.js
@@ -1,3 +1,42 @@
+const butContainer=document.querySelectorAll("button[id^=but]");
+let text2=document.getElementById("text2");
+let text1=document.getElementById("text1");
+//const but3=document.getElementById("but3");
+
+let displayValue1=0;
+let displayValue2=0;
+let dsplyVal=0;
+let equation=0;
+let op=null;
+let eqls=0;
+
+[].forEach.call(butContainer,function(e){
+  e.addEventListener("click",function(){
+    if(e.textContent==="+"){
+      op="add";
+      dsplyVal=displayValue1;//3
+      equation=displayValue1+"+";//3+
+      text1.textContent=equation;
+    }else if(e.textContent==="="){
+      equation=equation+"=";
+      text1.textContent=equation;
+      text2.textContent=eqls;
+      //alert(operate(op,dsplyVal,displayValue2));
+
+    }else{
+      displayValue2=parseInt(e.textContent);//displayValue2=3
+      if(op==="add"){
+        //alert(dsplyVal+" "+displayValue2);
+        eqls=operate(op,dsplyVal,displayValue2);
+      }
+      displayValue1=displayValue2;        //displayValue1=3
+      text2.textContent=displayValue2;
+    }
+  });
+});
+
+
+
 function add(...args){
   return args.reduce((total,a)=>total+a,0);
 }
@@ -11,19 +50,17 @@ function divide(...args){
   return args.reduce((total,a)=>total/a);
 }
 
+
 function operate(op,...args){
   switch(op){
-    case add:
-      add(...args);
-      break;
-    case sub:
-      sub(...args);
-      break;
-    case mul:
-      mul(...args);
-      break;
-    case divide:
-      divide(...args);
-      break;
+    case "add":
+      return add(...args);
+    case "sub":
+      return sub(...args);
+    case "mul":
+      return mul(...args);
+    case "divide":
+      return divide(...args);
   }
 }
+
